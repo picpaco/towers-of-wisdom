f6852bf1e64aa7c87e832c6ea13a94ca2172779b
Stefano <ortese89@hotmail.it>
2020-05-06 11:13:31 +0200

    pulizia repository

--- front-end/angular-Towers-of-wisdom/src/app/model/giocatore.spec.ts
+++
@@ -1,7 +0,0 @@
-import { Giocatore } from './giocatore';
-
-describe('Giocatore', () => {
-  it('should create an instance', () => {
-    expect(new Giocatore()).toBeTruthy();
-  });
-});
--- front-end/template-Towers-of-wisdom/Js/prova.js
+++
@@ -1,5 +0,0 @@
-$(document).ready(function () {
-    $("button#my").click(function () {
-        alert("Hello, world!");
-    });
-});
