6eea865a04da08d88eb337837d218779fa73bb5d
Stefano <ortese89@hotmail.it>
2020-05-08 10:27:16 +0200

    delete node_modules

--- front-end/angularclient/src/app/landing-page/login-page/login-page.component.ts
+++ front-end/angularclient/src/app/landing-page/login-page/login-page.component.ts
@@ -8,8 +8,10 @@ import { Component, OnInit } from '@angular/core';
 export class LoginPageComponent implements OnInit {
   username="";
   password="";
+  
 
   constructor() { }
+
 
   ngOnInit() {
   }
