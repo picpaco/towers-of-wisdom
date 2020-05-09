bbb00c9a78bf8094caebd5f13bfe12ee981e44c6
Arcadie <boisteanu19@gmail.com>
2020-05-10 00:08:41 +0200

    Aggiunto tasti 'disconettiti' e 'torna al menu' alla pagina di fine partita,pagina responsive,aggiunto stile.

--- front-end/angularclient/src/app/leaderboard-page/leaderboard-page.component.ts
+++ front-end/angularclient/src/app/leaderboard-page/leaderboard-page.component.ts
@@ -1,29 +1,26 @@
-import { Component, OnInit } from '@angular/core';
-import { Giocatore } from '../model/giocatore';
-import { GiocatoreService } from '../service/giocatore-service.service';
-
+import { Component, OnInit } from "@angular/core";
+import { Giocatore } from "../model/giocatore";
+import { GiocatoreService } from "../service/giocatore-service.service";
+import { EmailValidator } from "@angular/forms";
+import { getMaxListeners } from "process";
 
 @Component({
-  selector: 'app-leaderboard-page',
-  templateUrl: './leaderboard-page.component.html',
-  styleUrls: ['./leaderboard-page.component.css'],
+  selector: "app-leaderboard-page",
+  templateUrl: "./leaderboard-page.component.html",
+  styleUrls: ["./leaderboard-page.component.css"],
 })
 export class LeaderboardPageComponent implements OnInit {
-
+  
   public giocatori: Giocatore[];
+
 
   constructor(private giocatoreService: GiocatoreService) {}
 
   ngOnInit(): void {
-
     this.giocatoreService.findAll().subscribe((data) => {
-      
       this.giocatori = data;
-
     });
 
 
   }
-
-  
 }
