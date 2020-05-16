0cd3b5f9c84672d0ed360b432d30e99a6224297b
Arcadie <boisteanu19@gmail.com>
2020-05-17 01:28:22 +0200

    Installata libreria jQuery.

--- front-end/angularclient/src/app/match-page/match-page.component.ts
+++ front-end/angularclient/src/app/match-page/match-page.component.ts
@@ -1,17 +1,25 @@
-import { Component, OnInit } from '@angular/core';
- 
+import { Component, OnInit } from "@angular/core";
+import * as $ from "jquery";
 
 @Component({
-  selector: 'app-match-page',
-  templateUrl: './match-page.component.html',
-  styleUrls: ['./match-page.component.css']
+  selector: "app-match-page",
+  templateUrl: "./match-page.component.html",
+  styleUrls: ["./match-page.component.css"],
 })
 export class MatchPageComponent implements OnInit {
-
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {
-    
-  }
+    $(document).ready(function () {
+      $(".chat").hide();
 
+      $("button i.fa-comment-dots").click(function () {
+        $(".chat").show();
+      });
+
+      $(".chat button.close-chat").click(function () {
+        $(".chat").hide();
+      });
+    });
+  }
 }
