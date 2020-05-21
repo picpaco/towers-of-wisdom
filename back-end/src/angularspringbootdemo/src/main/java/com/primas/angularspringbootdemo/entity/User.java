145f8e03401184718c5c822ed2bba2f4546d1b97
Stefano <ortese89@hotmail.it>
2020-05-21 15:47:09 +0200

    aggiornamento User

--- back-end/src/angularspringbootdemo/src/main/java/com/primas/angularspringbootdemo/entity/User.java
+++ back-end/src/angularspringbootdemo/src/main/java/com/primas/angularspringbootdemo/entity/User.java
@@ -10,8 +10,14 @@ public class User {
 	
 	private String userName;
 	private String password;
+	private String status;
 	
 	public User() {
+		super();
+	}
+	
+	public User(String status) {
+		this.status=status;
 	}
 	
 	public User(String userName, String password) {
@@ -31,5 +37,15 @@ public class User {
 	public void setPassword(String password) {
 		this.password = password;
 	}
+
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+	
+	
 
 }
