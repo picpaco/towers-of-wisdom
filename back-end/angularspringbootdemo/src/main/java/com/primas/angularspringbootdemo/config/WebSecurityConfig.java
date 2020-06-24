6613769d3f0a5e232b5da855569534280307651c
GabZonno <10gabry10@gmail.com>
2020-06-24 16:23:19 +0200

    security datavase

--- back-end/angularspringbootdemo/src/main/java/com/primas/angularspringbootdemo/config/WebSecurityConfig.java
+++ back-end/angularspringbootdemo/src/main/java/com/primas/angularspringbootdemo/config/WebSecurityConfig.java
@@ -4,29 +4,17 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
-<<<<<<< HEAD
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
-=======
-import org.springframework.security.authentication.AuthenticationManager;
->>>>>>> f41992863c4efda20d2c06d4e932ab74784b85f5
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-<<<<<<< HEAD
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-=======
-import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
->>>>>>> f41992863c4efda20d2c06d4e932ab74784b85f5
 
 import com.primas.angularspringbootdemo.entity.UserDetailsServiceImpl;
 
@@ -35,7 +23,6 @@ import com.primas.angularspringbootdemo.entity.UserDetailsServiceImpl;
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
-<<<<<<< HEAD
 //	@Autowired
 //	DataSource dataSource;
 //
@@ -71,13 +58,6 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.authenticationProvider(authenticationProvider());
     }
-=======
-	@Autowired
-	private JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
-
-	@Autowired
-	private UserDetailsService jwtUserDetailsService;
->>>>>>> f41992863c4efda20d2c06d4e932ab74784b85f5
 
 	@Autowired
 	private JwtRequestFilter jwtRequestFilter;
@@ -102,7 +82,6 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 		return super.authenticationManagerBean();
 	}
 
-<<<<<<< HEAD
 //	@Autowired
 //	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
 //		auth.inMemoryAuthentication().withUser("stefano89").password("{noop}stefanorusso").roles("USER");
@@ -121,23 +100,4 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	//
 	//		return new InMemoryUserDetailsManager(user);
 	//	}
-=======
-	@Override
-	protected void configure(HttpSecurity httpSecurity) throws Exception {
-		// We don't need CSRF for this example
-		httpSecurity.csrf().disable()
-				// dont authenticate this particular request
-				.authorizeRequests().antMatchers("/authenticate").permitAll().antMatchers(HttpMethod.OPTIONS, "/**")
-				.permitAll().
-				// all other requests need to be authenticated
-				anyRequest().authenticated().and().
-				// make sure we use stateless session; session won't be used to
-				// store user's state.
-				exceptionHandling().authenticationEntryPoint(jwtAuthenticationEntryPoint).and().sessionManagement()
-				.sessionCreationPolicy(SessionCreationPolicy.STATELESS);
-
-		// Add a filter to validate the tokens with every request
-		httpSecurity.addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
-	}
->>>>>>> f41992863c4efda20d2c06d4e932ab74784b85f5
 }
