dfc15c3dacbafe37e1a07f1ab31da2c1f127e2d7
LucarinoManuel <64529140+Manuelsss@users.noreply.github.com>
2020-07-13 09:33:21 +0200

    Aggiornamento primi test

--- front-end/angularclient/src/app/login-page/login-page.component.spec.ts
+++ front-end/angularclient/src/app/login-page/login-page.component.spec.ts
@@ -2,16 +2,27 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
 import { DebugElement } from '@angular/core';
-
 import { LoginPageComponent } from './login-page.component';
+import { HttpClientModule } from '@angular/common/http';
+import { RouterTestingModule } from "@angular/router/testing";
+import { FormBuilder } from '@angular/forms';
+import { DataService } from '../landing-page/data.service';
+
+const loginServiceSpy = jasmine.createSpyObj('AuthenticationService', ['navigateByUrl']);
+const routerSpy = jasmine.createSpyObj('Router', ['login']);
+const dataSpy = jasmine.createSpyObj('DataService', ['login']);
 
 describe('LoginPageComponent', () => {
   let component: LoginPageComponent;
   let fixture: ComponentFixture<LoginPageComponent>;
 
   beforeEach(async(() => {
+    component = new LoginPageComponent(dataSpy, loginServiceSpy, routerSpy);
     TestBed.configureTestingModule({
-      declarations: [ LoginPageComponent ]
+      declarations: [ LoginPageComponent ],
+      imports:[HttpClientModule,
+        RouterTestingModule.withRoutes([]),
+      ],
     })
     .compileComponents();
   }));
@@ -22,5 +33,40 @@ describe('LoginPageComponent', () => {
     fixture.detectChanges();
   });
 
+  it('Il primo bottone dovrebbe essere il bottone Accedi', () => {
+    const linkDes = fixture.debugElement
+    .queryAll(By.css('Button'));
+    const nativeButton: HTMLButtonElement = linkDes[0].nativeElement;
+    expect(nativeButton.textContent).toBe(' Accedi ');
+  });
+  it('Component successfully created', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('component initial state', () => {
+    expect(component.invalidLogin).toBeFalsy();
+    expect(component.checkLogin).toBeDefined();
+    expect(component.updateData).toBeDefined();
+    expect(component.aggiornaStringa).toBeDefined();
+    expect(component.error).toBeFalsy();
+
+  });
+
+  /*it('submitted should be true when onSubmit()', () => {
+    component.onSubmit(blankUser);
+    expect(component.submitted).toBeTruthy();
+    expect(component.authError).toBeFalsy();
+  });
+
+  it('form value should update from when u change the input', (() => {
+    updateForm(validUser.username, validUser.password);
+    expect(component.loginForm.value).toEqual(validUser);
+  }));
+
+  it('Form invalid should be true when form is invalid', (() => {
+    updateForm(blankUser.username, blankUser.password);
+    expect(component.loginForm.invalid).toBeTruthy();
+  }));
+});*/
 
 });
